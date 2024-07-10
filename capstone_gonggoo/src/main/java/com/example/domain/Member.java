a5d1fb4707885eec67054d89f90480f40978bd0a
joyouji <a51189953@gmail.com>
2024-07-11 01:55:03 +0900

    공구친구

---
+++ capstone_gonggoo/src/main/java/com/example/domain/Member.java
@@ -0,0 +1,55 @@
+package com.example.domain;
+
+
+import com.example.groupbuying.domain.entity.Participant;
+import lombok.Builder;
+import com.example.mypage.domain.MyPage;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @NotBlank
+//    @Size(min = 5,max = 30)
+    private String loginId;
+
+    @NotBlank
+//    @Size(min = 5,max = 30)
+    private String password;
+
+    @NotBlank
+//    @Size(min = 3, max = 20)
+    private String name;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Participant> participants = new ArrayList<>();
+
+    public Member() {
+
+    }
+
+    @Builder
+    public Member(Long id, String loginId, String password, String name) {
+        this .id = id;
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+    }
+
+    public void addParticipant(Participant participant) {
+        participants.add(participant);
+        participant.setMember(this);
+    }
+}
---
+++ capstone_gonggoo/src/main/java/com/example/taxi/service/TaxiParticipantService.java
@@ -0,0 +1,155 @@
+package com.example.taxi.service;
+
+import com.example.domain.Member;
+import com.example.repository.MemberRepository;
+import com.example.taxi.domain.entity.Taxi;
+import com.example.taxi.domain.entity.TaxiParticipant;
+import com.example.taxi.domain.repository.TaxiRepository;
+import com.example.taxi.domain.repository.TaxiParticipantRepository;
+import com.example.taxi.dto.ParticipantDto;
+import com.example.taxi.dto.TaxiDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TaxiParticipantService {
+
+    private final TaxiParticipantRepository taxiParticipantRepository;
+    private final MemberRepository memberRepository;
+    private final TaxiRepository taxiRepository;
+
+    public TaxiParticipantService(TaxiParticipantRepository taxiParticipantRepository, MemberRepository memberRepository, TaxiRepository taxiRepository) {
+        this.taxiParticipantRepository = taxiParticipantRepository;
+        this.memberRepository = memberRepository;
+        this.taxiRepository = taxiRepository;
+    }
+
+    @Transactional
+    public Integer addParticipant(Integer taxiId, String loginId, boolean isRoomOwner) {
+
+        Taxi taxi = taxiRepository.findById(taxiId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. id=" + taxiId));
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다. loginId=" + loginId));
+
+//        int amountToAdd = taxi.getItemPrice() * quantity;
+//        taxi.addPrice(amountToAdd);
+
+        TaxiParticipant taxiParticipant = TaxiParticipant.builder()
+                .member(member)
+                .taxi(taxi)
+                .isRoomOwner(isRoomOwner)
+                .build();
+
+        taxiParticipantRepository.save(taxiParticipant);
+        return taxiParticipant.getRoomMemberId();
+    }
+
+//    @Transactional
+//    public void updateQuantity(Integer taxiId, String loginId, Integer newQuantity) {
+//        Taxi taxi = taxiRepository.findById(taxiId)
+//                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. id=" + taxiId));
+//        TaxiParticipant taxiParticipant = taxiParticipantRepository.findByTaxiRoomIdAndMember_LoginId(taxiId, loginId)
+//                .orElseThrow(() -> new IllegalArgumentException("참가자가 존재하지 않습니다. loginId=" + loginId));
+//    }
+
+    public List<TaxiParticipant> getParticipantsByTaxiId(Integer taxiId) {
+        Taxi taxi = taxiRepository.findById(taxiId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. id=" + taxiId));
+        return taxiParticipantRepository.findByTaxi(taxi);
+    }
+
+    public List<ParticipantDto> getParticipantDtosByTaxiId(Integer taxiId) {
+        Taxi taxi = taxiRepository.findById(taxiId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. id=" + taxiId));
+        List<TaxiParticipant> taxiParticipants = taxiParticipantRepository.findByTaxi(taxi);
+        return taxiParticipants.stream()
+                .map(ParticipantDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isUserParticipated(Integer taxiId, String loginId) {
+        return taxiParticipantRepository.existsByTaxiRoomIdAndMember_LoginId(taxiId, loginId);
+    }
+
+    public void removeParticipant(Integer taxiId, String loginId) {
+        TaxiParticipant taxiParticipant = taxiParticipantRepository.findByTaxiRoomIdAndMember_LoginId(taxiId, loginId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 참여 정보를 찾을 수 없습니다. taxiId: " + taxiId + ", loginId: " + loginId));
+        taxiParticipantRepository.delete(taxiParticipant);
+    }
+
+
+        
+    //서경원 추가부분
+    @Transactional(readOnly = true)
+    public List<TaxiDto> getParticipatedTaxiListByMemberId(Long memberId) {
+        return taxiParticipantRepository.findByMemberId(memberId).stream()
+            .map(participant -> {
+                Taxi taxi = participant.getTaxi();
+                String roomCategoryAsString = String.valueOf(taxi.getRoomCategory());
+                return new TaxiDto(
+                    taxi.getRoomId(),
+                    taxi.getAuthor(),
+                    taxi.getRoomTitle(),
+                    roomCategoryAsString,
+                    taxi.getRecruitNum(),
+                    taxi.getCurrentNum(),
+                    taxi.getPredicttotalprice(),
+                    taxi.getCreatedDate(),
+                    taxi.getModifiedDate(),
+                    taxi.getDeparture(),
+                    taxi.getDestination(),
+                    taxi.getMember()
+                );
+            })
+            .collect(Collectors.toList());
+    }
+
+
+
+
+//    @Transactional
+//    public boolean withdrawFromTaxi(Integer taxiId, String loginId) {
+//        try {
+//            Participant participant = participantRepository.findByTaxiRoomIdAndMember_LoginId(taxiId, loginId)
+//                    .orElseThrow(() -> new IllegalArgumentException("해당 참여 정보를 찾을 수 없습니다. taxiId: " + taxiId + ", loginId: " + loginId));
+//
+//            Taxi taxi = participant.getTaxi();
+//            int amountToSubtract = taxi.getItemPrice() * participant.getQuantity();
+//            taxi.subtractPrice(amountToSubtract);
+//
+//            participantRepository.delete(participant);
+//            return true;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+
+//    @Transactional
+//    public Integer editParticipantQuantity(Integer participantId, Integer newQuantity, String loginId) {
+//        Participant participant = participantRepository.findById(participantId)
+//                .orElseThrow(() -> new IllegalArgumentException("참여자 정보를 찾을 수 없습니다."));
+//
+//        if (!participant.getMember().getLoginId().equals(loginId)) {
+//            throw new IllegalStateException("수정 권한이 없습니다.");
+//        }
+//
+//        Taxi taxi = participant.getTaxi();
+//        int oldTotalPrice = participant.getQuantity() * taxi.getItemPrice();
+//        int newTotalPrice = newQuantity * taxi.getItemPrice();
+//
+//        taxi.updatePrice(taxi.getCurrentPrice() - oldTotalPrice + newTotalPrice);
+//        participant.setQuantity(newQuantity);
+//
+//        return taxi.getRoomId();
+//    }
+
+//    public void updateParticipantQuantity(String name, int newQuantity) {
+//        TaxiParticipant taxiParticipant = taxiParticipantRepository.findByMember_Name(name);
+//        if (taxiParticipant != null) {
+//            taxiParticipantRepository.save(taxiParticipant);
+//        }
+//    }
+
+}
+
